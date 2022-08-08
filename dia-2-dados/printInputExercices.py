21c46acb03945e2af5d0b415c0b9069b2908b5a0
vitor <vitorsbsilva@gmail.com>
2022-08-08 18:07:19 -0300

    feat: math.py, test_math.py

---
+++ dia-2-dados/printInputExercices.py
@@ -0,0 +1,6 @@
+# Exercício 1: Faça um programa que solicite o nome de uma pessoa usuária e imprima-o na vertical. Exemplo:
+
+name = input('Digite seu nome: ')
+
+for char in name:
+    print(char)
